59603dd575e89de8250d1e921f5cd810d3739256
Marcos Murillo <marcos.felix@ies2.com.br>
2020-02-18 08:48:00 -0300

    configurando eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello Redux</h1>
+  return (
+    <div className="App">
+      <h1>Hello Redux </h1>
+    </div>
+  );
 }
 
 export default App;
