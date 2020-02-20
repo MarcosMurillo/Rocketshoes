7db786f852221b237b254d3d59726a4d08fd89db
Marcos Murillo <marcos.felix@ies2.com.br>
2020-02-20 09:36:02 -0300

    configurando reactotron e reactotron-redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import './config/Reactotronconfig';
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
